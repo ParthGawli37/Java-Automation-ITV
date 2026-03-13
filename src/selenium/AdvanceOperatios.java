0acfe92c0bb83a1e1c5aa7b2cea3185d9d9bdccf
PG|37 <10junespider@gmail.com>
2026-03-13 17:57:49 +0530

    commit

---
+++ src/selenium/AdvanceOperatios.java
@@ -0,0 +1,54 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class AdvanceOperatios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Setting the chrome driver path for Java project to locate
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\parth\\chromedriver-win64\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		//
+		driver.get("https://testautomationpractice.blogspot.com/");
+		
+//		WebElement rbMale = driver.findElement(By.id("male"));
+//		rbMale.click();
+//		if(rbMale.isSelected()) {
+//			System.out.println("Male is selected");
+//		} else {
+//			System.out.println("Male is not selected");
+//		}
+		
+//		WebElement chkSunday = driver.findElement(By.id("sunday"));
+//		if(chkSunday.isSelected()) {
+//			System.out.println("Sunday is already selected");
+//		} else {
+//			chkSunday.click();
+//			System.out.println("Sunday is now selected");
+//		}
+	
+		Actions act = new Actions(driver);
+		WebElement btnPointMe = driver.findElement(By.className("dropbtn"));
+		WebElement srcDrag = driver.findElement(By.id("draggable"));
+		WebElement dstDrop = driver.findElement(By.id("Droppable"));
+		
+		act.dragAndDrop(srcDrag, dstDrop).perform();
+//		act.moveToElement(btnPointMe).perform();
+//		act.contextClick(btnPointMe).perform();
+		
+		
+		
+		
+
+	}
+
+}
--- src/selenium/BasicOperations.java
+++ src/selenium/BasicOperations.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 
 public class BasicOperations {
 
 	public static void main(String[] args) {
-		//Setting the chrome driver path for Java project to locate
+				//Setting the chrome driver path for Java project to locate
 				System.setProperty("webdriver.chrome.driver", "C:\\Users\\parth\\chromedriver-win64\\chromedriver.exe");
 				
 				WebDriver driver = new ChromeDriver();
@@ -18,10 +20,60 @@ public class BasicOperations {
 				//
 				driver.get("https://testautomationpractice.blogspot.com/");
 				
-				WebElement edtName = driver.findElement(By.id("name"));
+//				WebElement edtName = driver.findElement(By.id("name"));
+//				edtName.clear();
+//				edtName.sendKeys("Parth");
+//
+//				WebElement edtName = driver.findElement(By.cssSelector("#name"));
+//				WebElement edtName = driver.findElement(By.xpath(""));
 				
-				edtName.sendKeys("Parth");
-
+//				WebElement parentEdtName = driver.findElement(By.xpath("//input[@id='name']/parent::div"));
+				
+//				WebElement btnStart = driver.findElement(By.name("start")) //basic
+//				WebElement btnStart = driver.findElement(By.cssSelector("button[name='start']")); //Css
+//				//check if button is displayed
+//				if(btnStart.isDisplayed()) {
+//					if
+//					btnStart.click();
+//					System.out.println("Clicked on Start button");
+//				} else {
+//					System.out.println("Start button is not enable");
+//				}
+				
+//				btnStart.click();
+				
+//				WebElement edtEmail = driver.findElement(By.id("email"));
+//				System.out.println("The Email place holder " + edtEmail.getAttribute("placeholder"));
+				
+				
+//				WebElement edtPhone = driver.findElement(By.tagName("input"));
+//				
+//				WebElement InkHome = driver.findElement(By.linkText("Home"));
+//				InkHome.click();
+				
+//				WebElement InkOnline = driver.findElement(By.partialLinkText("Online"));
+//				InkOnline.click();
+				
+				
+//				WebElement lblPageHeader = driver.findElement(By.xpath("//h1[@class='title']"));
+//				System.out.println("the page header is - " + lblPageHeader.getText());
+//						
+				
+//				driver.quit();
+				
+//				Select drpCountry = new Select(driver.findElement(By.id("country")));
+//				drpCountry.selectByIndex(4);
+//				drpCountry.selectByValue("usa");
+//				drpCountry.selectByContainsVisibleText("India");
+				
+//				Select drpColors = new Select(driver.findElement(By.id("colors")));
+//				drpColors.selectByIndex(0);
+//				drpColors.selectByIndex(1);
+//				
+//				drpColors.deselectAll();
+				
+				
+				
 	}
 
 }
